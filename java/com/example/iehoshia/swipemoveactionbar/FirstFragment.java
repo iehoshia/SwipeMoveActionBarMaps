cd4acb47c5984a318f66900f54664e7329df4b33
iehoshia <jepgez@gmail.com>
2016-07-04 11:26:25 -0600

    Primera Actualización

---
+++ java/com/example/iehoshia/swipemoveactionbar/FirstFragment.java
@@ -0,0 +1,46 @@
+package com.example.iehoshia.swipemoveactionbar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FirstFragment extends Fragment {
+
+    // Store instance variables
+    private String title;
+    private int page;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+    }
+
+
+    public static FirstFragment newInstance(int page, String title) {
+        FirstFragment fragmentFirst = new FirstFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragmentFirst.setArguments(args);
+        return fragmentFirst;
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_first_fragment, container, false);
+        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
+        tvLabel.setText(page + " -- " + title);
+        return view;
+    }
+}
---
+++ java/com/example/iehoshia/swipemoveactionbar/SecondFragment.java
@@ -0,0 +1,45 @@
+package com.example.iehoshia.swipemoveactionbar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SecondFragment extends Fragment {
+    // Store instance variables
+    private String title;
+    private int page;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+    }
+
+
+    public static SecondFragment newInstance(int page, String title) {
+        SecondFragment fragment= new SecondFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_second_fragment, container, false);
+        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
+        tvLabel.setText(page + " -- " + title);
+        return view;
+    }
+}
